820c6be57a65219060b044c80c51f56e85f9002d
Omar Faruqe <faruqe.cse@gmail.com>
2021-03-23 20:17:41 +0600

    Function Calling

---
+++ Lecture4.java
@@ -0,0 +1,7 @@
+public class Lecture4{
+	public static void main(String[] args) {
+		System.out.println("Print this");
+		System.out.println("And this");
+		System.out.println("Again this");				
+	}
+}
